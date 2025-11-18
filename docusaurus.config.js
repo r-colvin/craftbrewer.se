ee33dba1dd826258661c202a60fc84759c31ac4b
Robert Colvin <github@skrapost.net>
2025-11-18 14:10:56 +0100

    update docusaurus to 3.9.2

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,8 +18,13 @@ const config = {
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
+  markdown: {
+    hooks: {
+      onBrokenMarkdownLinks: 'warn',
+    },
+  },
+
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
